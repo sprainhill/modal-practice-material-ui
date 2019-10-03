875e552f085aa81911e72afee4b69d2d1d27a335
Rory Murray <rory.christian.murray@gmail.com>
2019-10-03 16:54:45 -0400

    Update exercises i

--- cr-tutorial/src/App.js
+++ cr-tutorial/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Header, Footer } from "./components/layouts";
+import Exercises from "./components/exercises";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Exercises />
       <Footer />
     </div>
   );
--- cr-tutorial/src/components/layouts/Header.js
+++ cr-tutorial/src/components/layouts/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 const Header = () => {
   return (
-    <div>
-      <div> A header</div>
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="display4" color="inherit">
+          Exercise Database
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 };
 
