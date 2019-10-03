e46f86ac2248c60d022cd69f50d34270894466f8
Rory Murray <rory.christian.murray@gmail.com>
2019-10-03 17:59:59 -0400

    Create store

--- cr-tutorial/store.js
+++ cr-tutorial/store.js
@@ -1,3 +1,12 @@
 export const muscles = [
     'shoulders', 'chest', 'arms' 'back', 'legs'
 ]
+
+export const exercises = [
+    {
+        id: "",
+        title: "",
+        description: "",
+        muscles: ""
+    }
+]
