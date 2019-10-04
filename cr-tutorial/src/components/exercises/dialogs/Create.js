389dd493b34660b4e4aa2a0be03f282cddd49d86
Rory Murray <rory.christian.murray@gmail.com>
2019-10-04 18:35:24 -0400

    Add stylings to button

--- cr-tutorial/src/components/exercises/dialogs/Create.js
+++ cr-tutorial/src/components/exercises/dialogs/Create.js
@@ -1,25 +1,43 @@
 import React from "react";
 import {
   Button,
-  Form,
+  Fab,
   Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle
 } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
 
 export default props => {
-  <>
-    <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Create a New Exercise</DialogTitle>
-      <DialogContent>
-        <DialogContentText>Please fill out the form below.</DialogContentText>
-        <Form></Form>
-      </DialogContent>
-      <DialogActions>
-        <Button color="primary">Create</Button>
-      </DialogActions>
-    </Dialog>
-  </>;
+  const [open, setOpen] = React.useState(false);
+
+  const styles = {
+    button: {
+      flexGrow: 1
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Fab color="inherit" size="small">
+        <AddIcon />
+      </Fab>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Create a New Exercise</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Please fill out the form below.</DialogContentText>
+          <form></form>
+        </DialogContent>
+        <DialogActions>
+          <Button color="primary">Create</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 };
--- cr-tutorial/src/components/layouts/Header.js
+++ cr-tutorial/src/components/layouts/Header.js
@@ -2,14 +2,22 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import CreateDialog from "../exercises/dialogs/Create";
+
+const styles = {
+  header: {
+    flexGrow: 1
+  }
+};
 
 const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h2" color="inherit">
+        <Typography variant="h2" color="inherit" style={styles.header}>
           Exercise Database
         </Typography>
+        <CreateDialog />
       </Toolbar>
     </AppBar>
   );
