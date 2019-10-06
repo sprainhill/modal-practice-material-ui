b2f9b7fae58395560f7a4161765aaf53718fc2b4
Rory Murray <rory.christian.murray@gmail.com>
2019-10-06 16:27:09 -0400

    Cleanup files'
    '

--- cr-tutorial/src/components/exercises/RightPane.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Paper from "@material-ui/core/Paper";
-
-const RightPane = ({ styles }) => {
-  console.log("RP styles", styles);
-  return <Paper style={styles.Paper}>Right pane</Paper>;
-};
-
-export default RightPane;
